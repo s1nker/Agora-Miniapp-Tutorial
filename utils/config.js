74f521ee72398bb4ba0bcaa7ba08229da16b1f1c
sinker <s1nker@126.com>
2021-10-28 10:46:12 +0800

    修改 appid config

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
-const APPID = "272238450a764397b0aa3a10acbdda58";
+// const APPID = "272238450a764397b0aa3a10acbdda58";
+const APPID = "4a69f07b0bb54f1a9aca890958261e1f";
 
 if(APPID === ""){
   wx.showToast({
